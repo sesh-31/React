0742bc331497130b95d6df5c5a7a01c5f9293140
sesh-31 <paudelmukunda98@gmail.com>
2024-08-05 08:15:22 +0545

    Button.jsx , App.jsx , ProfilePicture.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,22 +1,11 @@
-import React from 'react';
-import List from './List.jsx'
-function App() {
+import Button from "./Button.jsx";
+import ProfilePicture from "./ProfilePicture.jsx";
+function app(){
 
-  const fruits=[{id:1 ,name:"apple",calories:95},
-                {id:2,name:"orange",calories:45},
-                {id:3,name:"banana",calories:105},
-                {id:4,name:"coconut",calories:159}];
-
-   const vegetables=[{id:5 ,name:"potatoes",calories:110},
-                {id:6,name:"celery",calories:15},
-                {id:7,name:"broccoli",calories:50},
-                {id:8,name:"carrot",calories:25}];
-
-return(
-  <>
-  {fruits.length>0? <List items={fruits} category="Fruits"/> :null}
-  {vegetables.length>0? <List items={vegetables} category="Vegetable"/> :null}
-</>
-);}
-
-export default App;
+  return(
+   
+    
+    <ProfilePicture/>
+  );
+}
+export default app;
